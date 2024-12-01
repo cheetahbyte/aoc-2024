a0bfd9ccb50f971295332dea9564208d72c2f1a8
Leo B. <me@leobreuer.dev>
2024-12-01 11:27:46 +0100

    day 1: add golang solution

---
+++ day01/main.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"bufio"
+	"os"
+	"slices"
+	"strconv"
+	"strings"
+)
+
+func readLineByLine() []string {
+	file, _ := os.Open("./day01/input.txt")
+	var lines []string
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		lines = append(lines, line)
+	}
+	return lines
+}
+
+func diff(a, b int) int {
+	if a < b {
+		return b - a
+	}
+	return a - b
+}
+
+func part1(left, right []int) {
+	distance := 0
+	for i := 0; i < len(left); i++ {
+		distance += diff(left[i], right[i])
+	}
+	println("Part 1:", distance)
+}
+
+func part2(left, right []int) {
+	similarityScore := 0
+	for _, element := range left {
+		appearances := 0
+		for _, rightElem := range right {
+			if rightElem == element {
+				appearances += 1
+			}
+		}
+		similarityScore += element * appearances
+	}
+	println("Part 2:", similarityScore)
+}
+
+func main() {
+	println("Hi")
+	lines := readLineByLine()
+	var left []int
+	var right []int
+	for _, element := range lines {
+		slice := strings.Split(element, "   ")
+		first, _ := strconv.Atoi(slice[0])
+		last, _ := strconv.Atoi(slice[1])
+		left = append(left, first)
+		right = append(right, last)
+	}
+	slices.Sort(left)
+	slices.Sort(right)
+	//
+	part1(left, right)
+	part2(left, right)
+}
